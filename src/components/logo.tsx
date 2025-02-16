da796710ddbe42a63c2db5ee246dc91e6282bf9f
Gianluca Laydner <gianlucaflaydner@gmail.com>
2025-02-16 19:38:30 -0300

    fix: logo size on mobile

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ const oswaldFont = Oswald({ subsets: ['latin'], variable: '--font-oswald' });
 export default function GianlucaLogo() {
   return (
     <h1
-      className={`${oswaldFont.variable} text-black font-oswald text-7xl logo`}
+      className={`${oswaldFont.variable} text-black font-oswald text-4xl md:text-7xl logo`}
     >
       {`<Gianluca />`}
     </h1>
