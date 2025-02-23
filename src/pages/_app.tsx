7db1dc782219e9a7c1c8926a16d90c1b71efbe27
Gianluca Laydner <gianlucaflaydner@gmail.com>
2025-02-23 15:39:43 -0300

    fix: head on app js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,42 @@
 import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Gianluca's portfolio showcasing projects and skills in web development."
+        />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+
+        <link rel="canonical" href="https://gianlucalaydner.dev" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Person',
+              name: 'Gianluca',
+              url: 'https://gianlucalaydner.dev',
+              sameAs: [
+                'https://github.com/gianlucaflaydner',
+                'https://www.linkedin.com/in/gianluca-laydner/',
+              ],
+            }),
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(App);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="pt">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Gianluca's portfolio showcasing projects and skills in web development."
-        />
-
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-
-        <link rel="canonical" href="https://gianlucalaydner.dev" />
-
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org',
-              '@type': 'Person',
-              name: 'Gianluca',
-              url: 'https://gianlucalaydner.dev',
-              sameAs: [
-                'https://github.com/gianlucaflaydner',
-                'https://www.linkedin.com/in/gianluca-laydner/',
-              ],
-            }),
-          }}
-        />
-      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
